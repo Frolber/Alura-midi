6c84585eb01cf4ea9c56decfc04aea661840bb06
Rodrigo <rodrigo.ldovr@gmail.com>
2023-02-25 13:57:48 -0300

    Ajustado o laço de repetição para for(antes while)

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let i = 0;
-
-while (i < listaDeTeclas.length) {
+for (let i =0; i < listaDeTeclas.length; i++) {
+   
    const tecla = listaDeTeclas[i];
+   
    const instrumento = tecla.classList[1];
+   
    const idAudio = `#som_${instrumento}`;
 
    tecla.onclick = function () {
       tocaSom(idAudio);
    };
-   i++;
 }
