993e7e14c964d321967218e865a3be6253d08dd7
Rodrigo <rodrigo.ldovr@gmail.com>
2023-02-26 10:53:46 -0300

    Melhoria aplicada especificando teclados para fazer a pintura e tratamento de erro na função tocaSom

--- main.js
+++ main.js
@@ -1,26 +1,42 @@
-function tocaSom(idElementoAudio) {
-   document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+   const elemento = document.querySelector(seletorAudio);
+
+   if (elemento !== null) {
+      console.log(elemento.localName === "audio");
+      if (elemento.localName === "audio") {
+         elemento.play();
+      } else {
+         console.log(`O elemento ${seletorAudio} não tem como tocar o som.`);
+      }
+   } else {
+      console.log(`O elemento ${seletorAudio} não existe.`);
+   }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-for (let i =0; i < listaDeTeclas.length; i++) {
-   
+for (let i = 0; i < listaDeTeclas.length; i++) {
    const tecla = listaDeTeclas[i];
-   
+
    const instrumento = tecla.classList[1];
-   
+
    const idAudio = `#som_${instrumento}`;
 
    tecla.onclick = function () {
       tocaSom(idAudio);
    };
 
-   tecla.onkeydown = function () {
-      tecla.classList.add('ativa');
-   }
+   tecla.onkeydown = function (event) {
+      if (
+         event.code === "Space" ||
+         event.code === "Enter" ||
+         event.code === "NumpadEnter"
+      ) {
+         tecla.classList.add("ativa");
+      }
+   };
 
    tecla.onkeyup = function () {
-      tecla.classList.remove('ativa');
-   }
+      tecla.classList.remove("ativa");
+   };
 }
